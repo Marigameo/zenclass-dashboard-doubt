8a97b1bf5158ef3a7ff6de1507e1b5d30bde1ebc
shyaminid <shyamini0306@gmail.com>
2022-01-17 23:26:56 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import "./App.css";
 // import { Route, Switch } from "react-router-dom";
 import { Sidebar } from "./Sidebar/sideBarcomponents";
 
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
 // import { makeStyles } from "@mui/styles";
 import { OtherComponent } from "./OtherComponent/OtherComponent";
 
